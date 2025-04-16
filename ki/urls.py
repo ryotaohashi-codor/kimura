d6882b58091812bf5fe35649ac84bdce83ec6e53
root <root@CODOR.>
2025-04-16 13:10:21 +0900

    a

--- ki/urls.py
+++ ki/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+def health_check(request):
+    return HttpResponse("OK", status=200)
+
 urlpatterns = [
-    path('', include('biz.urls')),
+    path('/', include('biz.urls')),
+    path('', health_check),
     path('admin/', admin.site.urls),
 ]
