860dcb688af8c2a054d03ea29293da7134d3c7a4
root <root@CODOR.>
2025-04-16 13:34:37 +0900

    a

--- biz/urls.py
+++ biz/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView
+from .views import ProjectListView, ProjectDetailView, DriverProjectListView
 
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('drivers/<int:pk>/projects/', DriverProjectListView.as_view(), name='driver_project_list'),
+
     ]
--- biz/views.py
+++ biz/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
-from .models import Project
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView
+from .models import Driver, Project
 
 class ProjectListView(ListView):
     model = Project
@@ -8,3 +10,14 @@ class ProjectListView(ListView):
 class ProjectDetailView(DetailView):
     model = Project
     template_name = 'project_detail.html'
+
+class DriverProjectListView(TemplateView):
+    template_name = 'driver_project_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        driver = get_object_or_404(Driver, pk=self.kwargs['pk'])
+        context['driver'] = driver
+        # ドライバーが関わったプロジェクトを重複なしで取得
+        context['projects'] = Project.objects.filter(deliveries__driver=driver).distinct()
+        return context
